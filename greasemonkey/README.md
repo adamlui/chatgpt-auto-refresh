895cf22fdfba0cffa544bbe6193f84559da226c2
adamlui@protonmail.com <adamlui@protonmail.com>
2023-03-28 16:43:52 -0700

    Synced from `adamlui/userscripts`

---
+++ greasemonkey/README.md
@@ -0,0 +1,76 @@
+# <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=23 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png"></picture> ChatGPT Auto Refresh ↻ &nbsp;[![Tweet](https://img.shields.io/twitter/url/http/shields.io.svg?style=social)](https://twitter.com/intent/tweet?text=Tired%20of%20constant%20interruptions%20on%20ChatGPT%3F%20Auto-refresh%20your%20sessions%20w%2F%20this%20plug-in%21%20&url=https://github.com/adamlui/userscripts/tree/master/chatgpt/chatgpt-auto-refresh&hashtags=greasemonkey,openai,chatgpt)
+
+[![license](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE.md)
+[![support](https://img.shields.io/badge/Support-Chrome/Firefox/Edge/Opera/Brave/Vivaldi/LibreWolf/QQ-989898.svg)](#compatibility)
+
+### Keeps ChatGPT sessions fresh to avoid Cloudflare checks.
+
+<a href="https://greasyfork.org/en/scripts/462422-chatgpt-auto-refresh"><img alt="Install this script" src="https://raw.githubusercontent.com/adamlui/userscripts/master/install-button.svg"></a><a href="#installation"><img alt="How to install" title="How to install" src="https://github.com/adamlui/userscripts/raw/master/help-button.svg"></a>
+<br>
+[Discuss](https://github.com/adamlui/userscripts/discussions) /
+[Review](https://greasyfork.org/en/scripts/462422-chatgpt-auto-refresh/feedback#post-discussion) /
+[Contact author](https://github.com/adamlui)
+
+#
+
+![image](https://user-images.githubusercontent.com/10906554/227314347-10671177-f485-4698-9931-2f6d0910ee54.png)
+![image](https://user-images.githubusercontent.com/10906554/227314471-cfd8362b-0662-479d-bb2e-5c13e2f622b0.png)
+
+
+## Installation
+
+1. Install Tampermonkey ([Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo), [Firefox](https://addons.mozilla.org/firefox/addon/tampermonkey/), [Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd), [Opera](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)) or Violentmonkey ([Chrome](https://chrome.google.com/webstore/detail/violent-monkey/jinjaccalgkegednnccohejagnlnfdag), [Firefox](https://addons.mozilla.org/firefox/addon/violentmonkey/), [Edge](https://microsoftedge.microsoft.com/addons/detail/violentmonkey/eeagobfjdenkkddmbclomhiblgggliao))
+
+2. Install [ChatGPT Auto Refresh](https://greasyfork.org/en/scripts/462422-chatgpt-auto-refresh) (will load in userscript manager installed above)
+
+3. Visit [chat.openai.com](https://chat.openai.com) as normal, and sessions will auto-refresh at a regular interval!
+
+## Dependencies
+
+ChatGPT Auto Refresh relies on code from the powerful [chatgpt.js](https://chatgptjs.org) library (c) 2023 Adam Lui & 冯不游 under the MIT license
+
+## Compatibility
+
+ChatGPT Auto Refresh is compatible with Chrome, Firefox, Edge, Opera, Brave, Vivaldi, LibreWolf & QQ using Tampermonkey or Violentmonkey. ([Pull requests](https://github.com/adamlui/userscripts/pulls) always welcome!)
+
+<a href="https://greasyfork.org/en/scripts/462422-chatgpt-auto-refresh"><img alt="Install this script" src="https://raw.githubusercontent.com/adamlui/userscripts/master/install-button.svg"></a><a href="#installation"><img alt="How to install" title="How to install" src="https://github.com/adamlui/userscripts/raw/master/help-button.svg"></a>
+<br>
+[Discuss](https://github.com/adamlui/userscripts/discussions) / 
+[Leave a review](https://greasyfork.org/en/scripts/462422-chatgpt-auto-refresh/feedback#post-discussion) /
+[More ChatGPT userscripts](https://github.com/adamlui/userscripts/tree/master/chatgpt)
+
+## Author links
+
+[![Follow @adamlui on GitHub](https://img.shields.io/github/followers/adamlui?label=Follow%20%40adamlui&style=social "GitHub")](https://github.com/adamlui)
+[![Follow @AdamLuiTube on YouTube](https://img.shields.io/youtube/channel/subscribers/UCgBMqK7SRL5R__3qM-YAcSg?label=Follow%20%40AdamLuiTube&style=social)](https://www.youtube.com/AdamLuiTube?sub_confirmation=1)
+<a href="https://elonsucks.org/@adam" target="_blank"><img align="bottom" src="https://img.shields.io/mastodon/follow/109387703022229926?domain=https%3A%2F%2Felonsucks.org&style=social" alt="Follow @adam on Mastodon" title="Mastodon">
+[![Follow @adamllui on Twitter](https://img.shields.io/twitter/follow/adamllui?style=social)](https://twitter.com/adamllui)
+<br><br>
+
+## Related scripts
+
+### <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=16 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png"></picture> [ChatGPT Widescreen Mode 🖥️](https://github.com/adamlui/userscripts/blob/master/chatgpt/chatgpt-widescreen-mode)
+
+Adds Widescreen + Full-Window modes to ChatGPT for reduced scrolling. // 向 ChatGPT 添加宽屏 + 全窗口模式以减少滚动。<br>
+[Install](https://greasyfork.org/en/scripts/461473-chatgpt-widescreen-mode) / 
+[Readme](https://github.com/adamlui/userscripts/blob/master/chatgpt/chatgpt-widescreen-mode/README.md) / 
+[Discuss](https://github.com/adamlui/userscripts/discussions)
+
+### <picture><source media="(prefers-color-scheme: dark)" srcset="https://i.imgur.com/RduASbD.png"><img width=16 src="https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png"></picture> [ChatGPT Heartbeat 💓](https://github.com/adamlui/userscripts/blob/master/chatgpt/chatgpt-heartbeat)
+
+Keeps conversations flowing when network errors are encountered. // 在遇到网络错误时保持对话流畅。<br>
+[Install](https://github.com/adamlui/userscripts/raw/master/chatgpt/chatgpt-heartbeat/ChatGPT-Heartbeat.user.js) / 
+[Readme](https://github.com/adamlui/userscripts/blob/master/chatgpt/chatgpt-heartbeat/README.md) / 
+[Discuss](https://github.com/adamlui/userscripts/discussions)
+
+### <img src="https://raw.githubusercontent.com/kudoai/bravegpt/main/media/images/bravegpt-icon48.png" width=18> [BraveGPT](https://github.com/kudoai/bravegpt/tree/main/greasemonkey) <a href="https://www.producthunt.com/posts/bravegpt?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-bravegpt" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=385630&theme=light" alt="BraveGPT - Bring&#0032;the&#0032;magic&#0032;of&#0032;ChatGPT&#0032;to&#0032;Brave&#0032;Search&#0033; | Product Hunt" style="width: 112px; height: 24px; margin:0 0 -4px 5px;" width="112" height="24" /></a>
+
+Display ChatGPT answers in Brave Search sidebar. // 在 Brave Search 侧边栏中显示 ChatGPT 答案。
+<br>[Install](https://greasyfork.org/en/scripts/462440-bravegpt) / 
+[Readme](https://github.com/kudoai/bravegpt/blob/main/greasemonkey/README.md) / 
+[Discuss](https://github.com/kudoai/bravegpt/discussions)
+
+#
+  
+<a href="https://github.com/adamlui/userscripts">**More userscripts**</a> / 
+<a href="#-chatgpt-auto-refresh--">Back to top ↑</a>
---
+++ greasemonkey/chatgpt-auto-refresh.user.js
@@ -0,0 +1,187 @@
+// ==UserScript==
+// @name             ChatGPT Auto Refresh ↻
+// @version          2023.03.28.2
+// @author           Adam Lui
+// @namespace        https://github.com/adamlui
+// @description      Keeps ChatGPT sessions fresh to avoid Cloudflare checks
+// @homepageURL      https://github.com/adamlui/userscripts
+// @supportURL       https://github.com/adamlui/userscripts/issues
+// @license          MIT
+// @match            https://chat.openai.com/chat*
+// @compatible       chrome
+// @compatible       firefox
+// @compatible       edge
+// @compatible       opera
+// @compatible       brave
+// @compatible       vivaldi
+// @compatible       librewolf
+// @compatible       qq
+// @icon             https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon48.png
+// @icon64           https://raw.githubusercontent.com/adamlui/userscripts/master/chatgpt/media/icons/openai-favicon64.png
+// @grant            GM_setValue
+// @grant            GM_getValue
+// @grant            GM_registerMenuCommand
+// @grant            GM_unregisterMenuCommand
+// ==/UserScript==
+
+// NOTE: This script uses code from the powerful chatgpt.js library @ https://chatgptjs.org (c) 2023 Adam Lui & 冯不游 under the MIT license.
+
+(function() {
+
+    // Import chatgpt.js functions
+
+    unsafeWindow.chatgptNotifyProps = { // NOTE: `unsafeWindow` is safe! `@match` is limited in scope.
+        // (Only if `@match` is highly inclusive, a malicious website could make userscripts do bad things.)
+        // Because this script uses GM methods (which isolates script in sandbox) `unsafeWindow` is
+        // required to create global object for notification positioning across scripts (but is safe!)
+        quadrants: { topRight: [], bottomRight: [], bottomLeft: [], topLeft: [] }
+    };
+    var chatGPTsessURL = 'https://chat.openai.com/api/auth/session';
+    var autoRefreshTimer = 60; // secs between session auto-refreshes
+    var chatgpt = {
+
+        activateAutoRefresh: function() {
+            if (!this.activateAutoRefresh.intervalId) {
+                console.info('↻ ChatGPT >> Auto refresh activated');
+                this.activateAutoRefresh.intervalId = setInterval(function() {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', chatGPTsessURL);
+                    xhr.send(); console.info('ChatGPT session refreshed');
+                }, autoRefreshTimer * 1000); // refresh every pre-set interval
+            } else { console.info('↻ ChatGPT >> Auto refresh already active!'); }
+        },
+
+        deactivateAutoRefresh: function() {
+            console.info('↻ ChatGPT >> ' + this.activateAutoRefresh.intervalId ?
+                'Auto refresh de-activated' : 'Refresher is not running!');
+            clearInterval(this.activateAutoRefresh.intervalId);
+        },
+
+        notify: function(msg, position, notifDuration) {
+            notifDuration = notifDuration ? +notifDuration : 1.75; // sec duration to maintain notification visibility
+            var fadeDuration = 0.6; // sec duration of fade-out
+            var vpYoffset = 13, vpXoffset = 27; // px offset from viewport border
+
+            // Make/stylize/insert div
+            var notificationDiv = document.createElement('div'); // make div
+            notificationDiv.style.cssText = ( // stylize it
+                '/* Box style */   background-color: black ; padding: 10px ; border-radius: 8px ; '
+                + '/* Visibility */  opacity: 0 ; position: fixed ; z-index: 9999 ; font-size: 1.8rem ; color: white');
+            document.body.appendChild(notificationDiv); // insert into DOM
+
+            // Determine div position/quadrant
+            notificationDiv.isTop = !position || !/low|bottom/i.test(position) ? true : false;
+            notificationDiv.isRight = !position || !/left/i.test(position) ? true : false;
+            notificationDiv.quadrant = (notificationDiv.isTop ? 'top' : 'bottom')
+                                     + (notificationDiv.isRight ? 'Right' : 'Left');
+
+            // Store div in global memory
+            unsafeWindow.chatgptNotifyProps.quadrants[notificationDiv.quadrant].push(notificationDiv);
+
+            // Position notification (defaults to top-right)
+            notificationDiv.style.top = notificationDiv.isTop ? vpYoffset.toString() + 'px' : '';
+            notificationDiv.style.bottom = !notificationDiv.isTop ? vpYoffset.toString() + 'px' : '';
+            notificationDiv.style.right = notificationDiv.isRight ? vpXoffset.toString() + 'px' : '';
+            notificationDiv.style.left = !notificationDiv.isRight ? vpXoffset.toString() + 'px' : '';
+
+            // Reposition old notifications
+            var thisQuadrantDivs = unsafeWindow.chatgptNotifyProps.quadrants[notificationDiv.quadrant];
+            if (thisQuadrantDivs.length > 1) {
+                var divsToMove = thisQuadrantDivs.slice(0, -1); // exclude new div
+                for (var j = 0; j < divsToMove.length; j++) {
+                    var oldDiv = divsToMove[j];
+                    var offsetProp = oldDiv.style.top ? "top" : "bottom"; // pick property to change
+                    var vOffset = +oldDiv.style[offsetProp].match(/\d+/)[0] + 5 + oldDiv.getBoundingClientRect().height;
+                    oldDiv.style[offsetProp] = `${vOffset}px`; // change prop
+            }}
+
+            // Show notification
+            notificationDiv.innerHTML = msg; // insert msg
+            notificationDiv.style.transition = 'none'; // remove fade effect
+            notificationDiv.style.opacity = 1; // show msg
+
+            // Hide notification
+            var hideDelay = ( // set delay before fading
+                fadeDuration > notifDuration ? 0 // don't delay if fade exceeds notification duration
+                : notifDuration - fadeDuration); // otherwise delay for difference
+            notificationDiv.hideTimer = setTimeout(function hideNotif() { // maintain notification visibility, then fade out
+                notificationDiv.style.transition = `opacity ${fadeDuration}s`; // add fade effect
+                notificationDiv.style.opacity = 0; // hide notification
+                notificationDiv.hideTimer = null; // prevent memory leaks
+            }, hideDelay * 1000); // ...after pre-set duration
+
+            // Destroy notification
+            notificationDiv.destroyTimer = setTimeout(function destroyNotif() {
+                notificationDiv.remove(); thisQuadrantDivs.shift(); // remove from DOM + memory
+                notificationDiv.destroyTimer = null; // prevent memory leaks
+            }, Math.max(fadeDuration, notifDuration) * 1000); // ...after notification hid
+        },
+
+        toggleAutoRefresh: function() {
+            if (!this.activateAutoRefresh.intervalId) {
+                console.info('↻ ChatGPT >> Auto refresh activated');
+                this.activateAutoRefresh.intervalId = setInterval(function() {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', chatGPTsessURL);
+                    xhr.send(); console.info('↻ ChatGPT >> ChatGPT session refreshed');
+                }, autoRefreshTimer * 1000); // refresh every pre-set interval
+            } else {
+                clearInterval(this.activateAutoRefresh.intervalId);
+                this.activateAutoRefresh.intervalId = null;
+                console.info('↻ ChatGPT >> Auto refresh de-activated');
+            }
+        }
+    };
+
+    // Define script functions
+
+    function registerMenu() {
+        var menuID = [] // to store registered commands for removal while preserving order
+        var stateSymbol = ['✔️', '❌'], stateWord = ['ON', 'OFF']
+        var stateSeparator = getUserscriptManager() === 'Tampermonkey' ? ' — ' : ': '
+
+        // Add command to toggle auto-refresh
+        var arLabel = stateSymbol[+config.arDisabled] + ' Auto-Refresh ↻ '
+                    + stateSeparator + stateWord[+config.arDisabled]
+        menuID.push(GM_registerMenuCommand(arLabel, function() {
+            chatgpt.toggleAutoRefresh()
+            saveSetting('arDisabled', !config.arDisabled)
+            if (!config.notifHidden) chatgpt.notify('Auto-Refresh: ' + stateWord[+config.arDisabled])
+            for (var i = 0 ; i < menuID.length ; i++) GM_unregisterMenuCommand(menuID[i]) // remove all cmd's
+            registerMenu() // serve fresh one
+        }))
+
+        // Add command to show notifications when switching modes
+        var mnLabel = stateSymbol[+config.notifHidden] + ' Mode Notifications'
+                    + stateSeparator + stateWord[+config.notifHidden]
+        menuID.push(GM_registerMenuCommand(mnLabel, function() {
+            saveSetting('notifHidden', !config.notifHidden)
+            chatgpt.notify('Mode Notifications: ' + stateWord[+config.notifHidden])
+            for (var i = 0 ; i < menuID.length ; i++) GM_unregisterMenuCommand(menuID[i]) // remove all cmd's
+            registerMenu() // serve fresh one
+        }))
+    }
+
+    function getUserscriptManager() {
+        try { return GM_info.scriptHandler } catch (error) { return "other" }}
+
+    function loadSetting() {
+        var keys = [].slice.call(arguments)
+        keys.forEach(function(key) {
+            config[key] = GM_getValue(configKeyPrefix + key, false)
+    })}
+
+    function saveSetting(key, value) {
+        GM_setValue(configKeyPrefix + key, value) // save to browser
+        config[key] = value // and memory
+    }
+
+    // Run main routine
+
+    var config = {}, configKeyPrefix = 'chatGPTar_'
+    loadSetting('arDisabled', 'notifHidden')
+    registerMenu() // create browser toolbar menu
+    if (!config.arDisabled) chatgpt.activateAutoRefresh()
+    if (!config.notifHidden) chatgpt.notify('Auto-Refresh: ' + (config.arDisabled ? 'OFF' : 'ON'))
+
+})()
